71b9153e08e58ae6404ea4ecaa4a2d5c4ce76bc4
Jing Davies <jing.davies@hotmail.com>
2021-05-14 14:16:10 +1000

    Table loaded

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,15 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//var tbody = d3.select("tbody")
+var ufo_tbody = d3.select("#ufo-table>tbody");
+
+//console.log(tableData);
+
+data.forEach((ufoData) => {
+    var row = ufo_tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
